f80a5b51e2d2bd0ca282cf9cc5cfb33fb1184cdd
mcverter <mitchell.verter@gmail.com>
2022-12-16 19:02:57 -0500

    ch 3 and 4

---
+++ scripts/ch03/packages.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/dash
+export LFS=/mnt/lfs
+
+PARTITION_LABEL="sda4"
+DISK_PARTITION=/dev/$PARTITION_LABEL
+
+echo "LFS " $LFS
+echo "DISK PARTITION " $DISK_PARTITION
+
+# mount -v ext4 $DISK_PARTITION $LFS
+#sudo mkdir -v $LFS/sources
+sudo chmod -v a+wt $LFS/sources
+echo `pwd`
+wget --input-file=$HOME/LFS/documentation/html/wget-list-sysv --continue --directory-prefix=$LFS/sources
+#pushd $LFS/sources
+#md5sum -c md5sums
+#popd
+echo $LFS
---
+++ scripts/ch04/initialize-lfs-system.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/dash
+export LFS=/mnt/lfs
+
+mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
+
+for i in bin lib sbin; do
+    ln -sv usr/$i $LFS/$i
+done
+
+case $(uname -m) in
+     x86_64) mkdir -pv $LFS/lib64 ;;
+esac
+
+mkdir -pv $LFS/tools
+
+	     
+
+
+    
