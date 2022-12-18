9b1160bfe0722f13b495d6f2acedb442d16c3806
mcverter <mitchell.verter@gmail.com>
2022-12-18 09:50:52 -0500

    more dependencies

--- scripts/ch01/install-converters.sh
+++ scripts/ch01/install-converters.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-sudo apt install libxml2 docbook tidy lynx xsltproc make m4
+sudo apt install libxml2 docbook tidy lynx xsltproc make m4 docbook-xsl libxml2-utils
 # sudo snap install  libxslt
