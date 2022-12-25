c5669ed7bdca027b67f4bef4710fdb60656ef133
mcverter <mitchell.verter@gmail.com>
2022-12-25 09:06:41 -0500

    updating

--- scripts/ch00/lfs_user_login.sh
+++ scripts/ch00/lfs_user_login.sh
@@ -1,14 +1,6 @@
 WHO=`whoami`
 
 if [ $WHO != "lfs" ] ; then
-    echo "Must be LFS user"
-    echo "Trying to log in ..."
-    su - lfs
-fi
-
-WHO=`whoami`
-
-if [ $WHO != "lfs" ] ; then
     echo "Current user is not LFS"
     echo "Exiting ..."
     exit 1
--- scripts/ch06/run_all.sh
+++ scripts/ch06/run_all.sh
@@ -1,6 +1,6 @@
 for file in `ls *.sh` ; do
     if [ $file != $0 ] ; then 
 	echo $file;
-	bash -xv $file
+	bash -xv $file || exit 1
     fi;
 done;
